72d7bad874290fd62b687637510eb0fdd86b4710
shijinke1990 <shijinke1990@qq.com>
2024-06-20 09:32:27 +0800

    chore: Update Start page with new links and padding

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -10,10 +10,10 @@ const Start = () => {
       <img className={styles.logo} src='/logo.png' alt='logo' />
       <ProductList></ProductList>
       <div className={styles.beiAn}>
-        <a href='http://beian.miit.gov.cn' target='_blank' rel='noopener noreferrer'>
+        <a className={styles.link} href='http://beian.miit.gov.cn' target='_blank' rel='noopener noreferrer'>
           备案号：鄂ICP备14002884号-6
         </a>
-        <a href='https://www.boluobo.com' target='_blank' rel='noopener noreferrer'>
+        <a className={styles.link} href='https://www.boluobo.com' target='_blank' rel='noopener noreferrer'>
           友情链接：菠萝卜
         </a>
       </div>
